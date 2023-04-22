9d35d8ee09886f77b7e1ed87548878676a2a28e9
TheDanieLSh <95643914+TheDanieLSh@users.noreply.github.com>
2023-04-22 07:55:50 +0300

    return error

--- anime_paradise/src/components/GoodsList.js
+++ anime_paradise/src/components/GoodsList.js
@@ -1,19 +1,34 @@
+import { useState, useEffect } from "react";
+
 function GoodsList(props) {
-    fetch('products.json')
-        .then(async (response) => {
-            const products = await response.json();
-            const goods = props.goods;
+    const [products, setProducts] = useState(null);
+    const goods = props.goods;
+    let productCards = [];
+
+    fetch('products.json').then(async response => {
+        setProducts(await response.json());
+    })
+    
+    useEffect(() => {
+        if (products != null) {
             for (let x = 0; x < products[goods].length; x++) {
-                return (
-                    <div className="list">
-                        <div className="productCard">
-                            <div className="productPicture"></div>
-                            <div className="productName"></div>
-                        </div>
+                productCards.push(
+                    <div className="productCard" key={x}>
+                        <div className="productPicture">
+                            <img src={products[goods[x].pic]}/></div>
+                        <div className="productName">{products[goods[x].name]}</div>
                     </div>
                 )
             }
-        })
+
+            return (
+                <div className="list">
+                    {productCards}
+                </div>
+            );
+        }
+    }
+    )
 }
 
 export default GoodsList;
