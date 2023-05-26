e79b85a71ec0f7b951eb66f3ece3659704ef3ba8
TheDanieLSh <95643914+TheDanieLSh@users.noreply.github.com>
2023-05-26 17:48:25 +0300

    main page pics done

--- anime_paradise/src/components/Sections.js
+++ anime_paradise/src/components/Sections.js
@@ -21,27 +21,6 @@ function Sections() {
             </>} />
         </Routes>
     )
-
-    // const { section } = useParams();
-    // if (section === '') {
-    //     return (
-    //         <div className="pageContent" data-main="true">
-    //             <p>Новинки</p>
-    //             <GoodsList goods="novelties" />
-    //             <p>Топ продаж</p>
-    //             <GoodsList goods="hot" />
-    //         </div>
-    //     )
-    // } else return (
-    //     <div className='pageContent'>
-    //         <Routes>
-    //             <Route path='/:section/:page_number' element={<>
-    //                 <GoodsList goods={section} />
-    //                 <Pages goods={section} />
-    //             </>} />
-    //         </Routes>
-    //     </div>
-    // )
 }
 
 export default Sections;
--- anime_paradise/src/index.js
+++ anime_paradise/src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/reduxStateStore';
 import './index.css';
-import Adminka from './components/Adminka';
 import SearchBar from './components/SearchBar';
 import Logo from './components/Logo';
 import Menu from './components/Menu';
@@ -17,31 +16,20 @@ root.render(
   <React.StrictMode>
     <Router>
       <Provider store={store}>
-        <Routes>
-          <Route path="*" element={<App />} />
-          <Route path="/adminka" element={<Adminka />} />
-        </Routes>
+        <header>
+          <SearchBar />
+          <Logo />
+          <Menu />
+        </header>
+        <main>
+          <Sections />
+        </main>
+        <footer>
+          <Bottom />
+        </footer>
       </Provider>
     </Router>
   </React.StrictMode>
 );
-
-function App() {
-  return (
-    <div>
-      <header>
-        <SearchBar />
-        <Logo />
-        <Menu />
-      </header>
-      <main>
-          <Sections />
-      </main>
-      <footer>
-        <Bottom />
-      </footer>
-    </div>
-  )
-}
 
 reportWebVitals();
