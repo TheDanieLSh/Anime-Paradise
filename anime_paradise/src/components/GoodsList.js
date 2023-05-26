384e0160f19fde2e37c7329e38d80ff2a3c49e31
TheDanieLSh <95643914+TheDanieLSh@users.noreply.github.com>
2023-05-26 13:10:32 +0300

    dynamic pages is done!

--- anime_paradise/src/components/GoodsList.js
+++ anime_paradise/src/components/GoodsList.js
@@ -12,22 +12,28 @@ function GoodsList(props) {
         }
         return params.section
     }
-    
+
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(fetchProducts());
     }, [dispatch])
-    
+
     const products = useSelector(state => state.fetchDataReducer.products);
 
-    let firstProduct;
-    let lastProduct;
-    if ()
-    const currentPageProducts = products.
-    
+    const firstProduct = (params.page_number - 1) * 8;
+    const lastProduct = firstProduct + 8;
+    let currentPageProducts;
+    if (products) {
+        if (products[goods].length > 8) {
+            currentPageProducts = products ? products[goods].slice(firstProduct, lastProduct) : [];
+        } else {
+            currentPageProducts = products[goods];
+        }
+    }
+
     return (
         <div className="list">
-            {products && products[goods].map((good, i) => (
+            {products && currentPageProducts.map((good, i) => (
                 <div className="productCard" key={i}>
                     <div className="productPicture">
                         <img src={good.pic} alt="ОШИБКА" />
